ab786dc6571eac2a467ea2bc849ace7d449529e1
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-07-08 18:15:37 +0200

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/* wrapping all global variables in 'Immediately Invoked Function Expression (or IIFE)' to avoid external code conflicts */
+var pokemonRepository = (function() { //This is the IIFE wrap
+      
+})(); // Wrapping IIFE end
+
+// outside IIFE-Wrap

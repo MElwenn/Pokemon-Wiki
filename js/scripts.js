a587576b40b7d1b4b53ae9748e626a184dc8a9ae
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-07-19 21:22:29 +0200

    Update scripts.js
    added filter to the search results within added toggle and id to the pthe search box, okemon button, removed the appends, added queries to HTML structure

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var pokemonRepository = (function() { //This is the IIFE wrap
     var pokemonList = [];
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //define apiURL
     var $response = $('.pokemon-list');
-    var $modalContainer = $('#modal-container');
+    var $modalContainer = $('.modal-container'); //(replaced all # with .)
 
     function add (pokemon) { // function to load the list of Pokemons
         pokemonList.push(pokemon);
@@ -23,10 +23,13 @@ var pokemonRepository = (function() { //This is the IIFE wrap
     function addListItem(pokemon) { //addListItem
 
         var $listItem = $('<div class="list-group"></div>');
-        var $button = $('<button type="button" class="list-group-item list-group-item-action">'+ capitalize(pokemon.name) + '</button>');
+        var $button = $('<button type="button" class="list-group-item list-group-item-action data-toggle="modal" data-target="#modal-container">'+ capitalize(pokemon.name) + '</button>');
         $response.append($button);
 
-        $button.click(function(){
+//        $button.click(function(){
+//            showDetails(pokemon);
+//        });
+        $button.on('click', function(){
             showDetails(pokemon);
         });
     } //addListItem end
@@ -77,43 +80,55 @@ var pokemonRepository = (function() { //This is the IIFE wrap
 
     function showModal(pokemon) {
         // Clear all existing modal content
-        $modalContainer.empty();
+//        $modalContainer.empty();
 
-        //This is the new Bootstrap-modal
-        var modal = $('#modal-container'); // transparent background covering the entire screen
-            var modalDialog = $('#modalDialog'); // modal box that appears on top of the background
-            var modalContent = $('#modalContent');  // Wrap around the modal content
+        //This is the new Bootstrap-modal (replaced all # with .)
+//        var modal = $('#myModal').modal(options); // according to https://getbootstrap.com/docs/4.2/components/modal/
+        var modal = $('.modal-container'); // transparent background covering the entire screen
+            var modalDialog = $('.modalDialog'); // modal box that appears on top of the background
+            var modalContent = $('.modalContent');  // Wrap around the modal content
 
                 //This is the modal header
-                var modalHeader = $('#modalHeader');
-                    var titleElement = $('#pokemon-name').text(pokemon.name);
-                    var closeButtonElementHeader = $('#modalCloseHeader');
-                modalHeader.append(titleElement);
-                modalHeader.append(closeButtonElementHeader);
+                var modalHeader = $('.modalHeader');
+//                    var titleElement = $('.pokemon-name').text(pokemon.name);
+                    var titleElement = $('<h5>' + pokemon.name + '</h5>');
+                    var closeButtonElementHeader = $('.modalCloseHeader');
+//                modalHeader.append(titleElement);
+//                modalHeader.append(closeButtonElementHeader);
 
                 //This is the modal body
-                var modalBody = $('#modalBody');
-                    var heightElement = $('#pokemon-height' + pokemon.height * 10 + 'cm');
-                    var weightElement = $('#pokemon-weight' + pokemon.weight * 10 + 'cm');
-                modalBody.append(heightElement);
-                modalBody.append(weightElement);
+//                var modalBody = $('.modalBody');
+                var $modalBody = $('.modal-body');
 
-                var imageElement = $('<img class="img"/>').ready(pokemon);
-                imageElement.attr('src', pokemon.imageUrl);
-                modalBody.append(imageElement);
+                $modalBody.html('Height: ' + pokemon.height * 10 + ' cm' + '<br/><br/>' +
+                                'Weight: ' + pokemon.weight / 10 + ' g' + '<br/><br/>'
+
+                );
+
+//                    var heightElement = $('.pokemon-height' + pokemon.height * 10 + 'cm');
+/*                    var heightElement = $('<p>Height: ' + pokemon.height * 10 + ' cm</p>'); */ //attempt to "querying the existing "skeleton" and adding elements to it"
+//                    var weightElement = $('.pokemon-weight' + pokemon.weight * 10 + 'cm');
+/*                    var weightElement = $('<p>Weight: ' + pokemon.weight * 10 + ' g</p>'); */  //attempt to "querying the existing "skeleton" and adding elements to it"
+//                modalBody.append(heightElement);
+//                modalBody.append(weightElement);
+
+//                var imageElement = $('<img class="img"/>').ready(pokemon);
+//                imageElement.attr('src', pokemon.imageUrl);
+                    var imageElement = $("<img class='modal-image' src='" + pokemon.imageUrl + "'>");
+                    $modalBody.append(imageElement);
 
                 //This is the modal footer
                 var modalFooter = $('#modalFooter');
                     var closeButtonElementFooter = $('#modalCloseFooter');
-                modalFooter.append(closeButtonElementFooter);
+//                modalFooter.append(closeButtonElementFooter);
 
-            modalContent.append(modalHeader); //nedded to have influence on position of contained elements?
-            modalContent.append(modalBody);   //nedded to have influence on position of contained elements?
-            modal.append(modalFooter);        //nedded to have influence on position of contained elements?
-        modal.append(modalDialog);            //nedded to have influence on position of contained elements?
-        modal.append(modalContent);           //nedded to have influence on position of contained elements?
+//            modalContent.append(modalHeader); //nedded to have influence on position of contained elements?
+//            modalContent.append(modalBody);   //nedded to have influence on position of contained elements?
+//            modal.append(modalFooter);        //nedded to have influence on position of contained elements?
+//        modal.append(modalDialog);            //nedded to have influence on position of contained elements?
+//        modal.append(modalContent);           //nedded to have influence on position of contained elements?
 
-        $modalContainer.append(modal);
+//        $modalContainer.append(modal);
         $modalContainer.addClass("is-visible");
 
     } // showModal end
@@ -129,7 +144,7 @@ var pokemonRepository = (function() { //This is the IIFE wrap
         loadList: loadList,
         loadDetails: loadDetails, //add loadDetails
         showModal: showModal,
-        searchBox: searchBox
+//        searchBox: searchBox
     }; // return end
 
 })(); // Wrapping IIFE end
@@ -142,8 +157,25 @@ pokemonRepository.loadList().then(function(){
 })
 
 // Search-Box functionalities
+// filter search results using the searchBox
+function filterResult() { // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
+    var input, filter, ul, $response, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    $response = ul.getElementsByTagName("$response");
+    for (i = 0; i < $response.length; i++) {
+        a = $response[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            $response[i].style.display = ""; // $response.display, $button.display, $listItem.display or pokemon.display do not work here
+        } else {
+            $response[i].style.display = "none";  // $response.display, $button.display, $listItem.display or pokemon.display do not work here
+        }
+    }
+}
 //$(document).ready(function(){
-    $('#searchBox').on('keyup', function(){
+    $('#myInput').on('keyup', function filterResult(){
         var value = $(this).val().toLowerCase();
         $('.titleElement').filter(function(){
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
